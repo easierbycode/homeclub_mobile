44b828db387540afe55c2df86f3342088b24562b
Daniel Johnson <navseeker7@gmail.com>
2013-10-29 22:45:40 -0700

    get alerts

--- www/js/models.js
+++ www/js/models.js
@@ -1,10 +1,10 @@
 pushNotificationApp.factory('AllAlerts', function($q) {
-  var deferred = $q.defer();
+  var d = $q.defer();
   
   get('http://alerts.homeclub.us/alerts', null, function(err, resp) {
-    if(err) return deferred.reject(err);
-    deferred.resolve(JSON.parse(resp));
+    if(err) return d.reject(err);
+    d.resolve(JSON.parse(resp));
   });
   
-  return deferred.promise;
-})
+  return d.promise;
+});
