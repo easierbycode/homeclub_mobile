d64daae8f1076bae508493c57dd472002b9ba7dd
Daniel Johnson <navseeker7@gmail.com>
2013-11-05 15:43:17 -0700

    update pushplugin version

--- www/js/models.js
+++ www/js/models.js
@@ -5,7 +5,6 @@ pushNotificationApp.factory('Alert', function($q) {
   
       get('http://alerts.homeclub.us/alerts', null, function(err, resp) {
         if(err) return d.reject(err);
-        alert(resp);
         d.resolve(JSON.parse(resp));
       });
       
